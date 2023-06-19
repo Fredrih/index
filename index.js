6b181b6bfb97f5e261a87132026c0ccde615c3ea
Fredrih <136816993+Fredrih@users.noreply.github.com>
2023-06-19 17:16:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const  clientsEstimations=[];
+
+function  askClientToGiveEstimation(){
+    let customerRating=+prompt('Как вы оцениваете нашу кофейню от 1 до 10?');
+    if(customerRating>=0 && customerRating<=10){
+        clientsEstimations.push(customerRating);
+    }
+}
+
+for(let i=0;i<5;i++){
+    askClientToGiveEstimation();
+}
+
+let positivRating=clientsEstimations.filter((rating)=>{
+    return rating>5;
+});
+
+let negativRating=clientsEstimations.filter((rating)=>{
+    return rating<=5;
+});9
+
+
+console.log(positivRating);
+console.log(negativRating);
+
+alert(`Всего положительных оценок: ${positivRating.length}`);
+alert(`Всего отрицательных оценок: ${negativRating.length}`);
 
